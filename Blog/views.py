281eb8f204713db6e6985cbad1e1f6342c4834bf
ProgramadorGod <pipenet12@gmail.com>
2023-09-10 18:56:04 -0500

    Fine fine fine

--- Blog/views.py
+++ Blog/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-from Blog.models import Post
+from Blog.models import Post, Categories
 
 # Create your views here.
 def Blog(request):
     Posts = Post.objects.all()
-    return render(request,'Blog/Blog.html', {"Posts":Posts})
+    
+    # Crear una lista para almacenar las categorías únicas
+    unique_categories = []
+    
+    for post in Posts:
+        categories = post.Categories.all()
+        for category in categories:
+            # Verificar si la categoría ya está en la lista
+            if category not in unique_categories:
+                unique_categories.append(category)
+    
+    return render(request, 'Blog/Blog.html', {"Posts": Posts, "Categories": unique_categories})
+
+
+#def Category(request):
+    Categoriez = Categories.objects.all()
+    return render(request,'Blog/Blog.html', {"Categoriez":Categoriez})
+
