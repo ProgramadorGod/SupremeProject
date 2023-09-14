dcbe25e96c977cb2543f154a75a8d4dd239f75b6
ProgramadorGod <pipenet12@gmail.com>
2023-09-14 01:52:26 -0500

    Working Decent

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,6 +4,5 @@ from Blog import views
 
 urlpatterns = [
     path('', views.Blog, name='Blog'),
-    
 ]
 
--- Blog/views.py
+++ Blog/views.py
@@ -4,12 +4,11 @@ from Blog.models import Post, Categories
 # Create your views here.
 def Blog(request):
     # Obtén todas las publicaciones y categorías
+    posts = Post.objects.all()
     category_id = request.GET.get('category')
     if category_id:
         posts = Post.objects.filter(Categories = category_id)
-    else:
-        posts = Post.objects.all()
-
+    categories = Categories.objects.all()
     
     return render(request, 'Blog/Blog.html', {"Posts": posts, "Categories": categories, "ID":category_id})
 
