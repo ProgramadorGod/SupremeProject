5c4487e6fae68dab8b99732ca64e46a5646c9c2f
ProgramadorGod <pipenet12@gmail.com>
2023-09-26 21:06:06 -0500

    going to work

--- ShoppingCart/context_processor.py
+++ ShoppingCart/context_processor.py
@@ -1,8 +1,9 @@
 # context_processor.py
 def Car_Total_Import(request):
     total = 0
-
-    shopping_cart = request.session.get("ShoppingCart", {})
-    for value in shopping_cart.values():
-        total += float(value["Price"]) * value["Mount"]
+    print("LOOL ", request.user.is_authenticated)
+    if request.user.is_authenticated:
+        shopping_cart = request.session.get("ShoppingCart", {})
+        for value in shopping_cart.values():
+            total += float(value["Price"]) * value["Mount"]
     return {"Car_Total_Import": total}
--- authentication/views.py
+++ authentication/views.py
@@ -3,13 +3,17 @@ from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
 from django.http.response import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import check_password
+from django.contrib.auth import login
+
 
 # Create your views here.
 
 
 class VRegister(View):
     def get(self, request):
+        print(request.user)
         return render(request,'authentication/register.html')
         #form = UserCreationForm()
         #return render(request,'authentication/register.html', {"form":form})
@@ -19,11 +23,14 @@ class VRegister(View):
         password = request.POST.get("password")
         user = User.objects.filter(username=user_name).first()
 
-
         if user:
             if check_password(password, user.password):
-                return HttpResponse("Autenticated!!!")
-            
+                authenticated_user = authenticate(request,username=user_name,password=password)
+                if authenticated_user is not None:
+                    login(request, authenticated_user)  # Establece la sesión de usuario
+
+                    return HttpResponse("Autenticated!!!")
+                
             return HttpResponse("Invalid password.")
 
         return HttpResponse("User not found")
